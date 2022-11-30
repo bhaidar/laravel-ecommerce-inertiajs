7ba7f2caaa8a9d8d5b2d007d476adccd9beb5530
Bilal <bhaidar@gmail.com>
2022-12-01 00:02:17 +0200

    Adapt authenticated layout to be used as app layout

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+
+    <Head title="Welcome" />
+
+    <AppLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Home</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">List of categories</div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
