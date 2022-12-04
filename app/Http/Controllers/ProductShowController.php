b22800f212b70def8009c077c5de1d5c534b0542
Bilal <bhaidar@gmail.com>
2022-12-04 21:38:48 +0200

    Add products show page

---
+++ app/Http/Controllers/ProductShowController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Inertia\Inertia;
+
+class ProductShowController extends Controller
+{
+    public function __invoke(Product $product): \Inertia\Response
+    {
+        return Inertia::render('Products/Show', [
+            'product' => $product
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductShowController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', HomeController::class)->name('home');
+
+Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
