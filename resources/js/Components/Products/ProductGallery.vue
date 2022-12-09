17fa64c212988c113e7f92e02a7f01f1dbe786a1
Bilal <bhaidar@gmail.com>
2022-12-10 00:38:18 +0200

    display thumbnails and original photo and allow user to switch between them

--- resources/js/Components/Products/ProductGallery.vue
+++ resources/js/Components/Products/ProductGallery.vue
@@ -1,16 +1,27 @@
 <script setup>
+import { computed, ref } from 'vue';
+
 const props = defineProps({
   product: Object,
 });
+
+const original = ref(props.product?.media?.[0]?.original);
+const medias = computed(() => props.product?.media);
+
+const thumbnail = (media) => media?.conversions?.[0];
+
+const selectMedia = (media) => {
+  original.value = media.original;
+}
 </script>
 
 <template>
   <div class="flex flex-col space-y-4">
-    <img src="" alt="">
+    <img :src="original" alt="">
     <div class="grid grid-cols-6 gap-2">
-      <a href="#">
-        <img src="" alt="">
-      </a>
+      <button v-for="media in medias" :key="media" @click="selectMedia(media)">
+        <img :src="thumbnail(media)" alt="">
+      </button>
     </div>
   </div>
 </template>
--- resources/js/Pages/Products/Show.vue
+++ resources/js/Pages/Products/Show.vue
@@ -13,13 +13,12 @@ const formattedPrice = computed(() => props.product?.price?.formatted);
 
 <template>
   <AppLayout>
-    {{ product }}
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200 grid grid-cols-2 gap-4">
             <div class="col-span-1 grid">
-              <product-gallery :product="product" />
+              <product-gallery :product="product" class="mr-4" />
             </div>
             <div class="col-span-1 p-6 space-y-6">
               <div>
