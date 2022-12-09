f6d110e14033c87d312884a7297eda74f9da29da
Bilal <bhaidar@gmail.com>
2022-12-09 14:12:13 +0200

    Add media conversions to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,23 @@ namespace App\Models;
 use App\Console\Traits\HasFormattedPrice;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 /**
  * @property mixed $id
  */
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
     use HasFormattedPrice;
+    use InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
+    }
 }
