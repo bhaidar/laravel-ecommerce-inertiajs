a9aecb0433fd4fd5cbec304c5530b67b68eb9eeb
Bilal <bhaidar@gmail.com>
2023-01-09 15:30:59 +0200

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,13 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
+        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(VariationSeeder::class);
         $this->call(ShippingTypeSeeder::class);
